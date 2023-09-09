42ddf933505571a50c35f32131f621c1e81a3f6a
Jeetix <jeetix@gmail.com>
2023-09-09 18:17:14 +0000

    typo in instructions

--- index.php
+++ index.php
@@ -92,7 +92,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <ul>
                 <li>Use the arrow keys to move around the world.</li>
                 <li>Click on a block type to select it. (This also places block at player location)</li>
-                <li>You will keep placing selected block as you move until you cnage it.</li>
+                <li>You will keep placing selected block as you move until you change it.</li>
             </ul>
             <h3>Tools</h3>
             <ul>
