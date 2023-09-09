c85e9b50342241497e21facfd66aefa34e6d98f2
Jeetix <jeetix@gmail.com>
2023-09-09 19:07:59 +0000

    left panel (grid1) separated.

---
+++ left.php
@@ -0,0 +1,13 @@
+<h3>Instructions:</h3>
+<ol>
+    <li>Use the arrow keys to move around the world.</li>
+    <li>Click on a block type to select it. (This also places block at player location)</li>
+    <li>You will keep placing selected block as you move until you change it.</li>
+</ol>
+<h3>Tools</h3>
+<ul>
+    <li><a href="tools/worldphoto.php?world=<?php echo $playerWorld; ?>">World Photo</a></li>
+    <li><a href="tools/worldphoto.php?username=<?php echo $username; ?>&world=<?php echo $playerWorld; ?>">Your World Photo</a></li>
+    <li><a href="tools/worldphoto2.php?world=<?php echo $playerWorld; ?>">World Photo</a> (Experimental)</li>
+    <li><a href="tools/worldphoto2.php?username=<?php echo $username; ?>&world=<?php echo $playerWorld; ?>">Your World Photo</a> (Experimental)</li>
+</ul>
