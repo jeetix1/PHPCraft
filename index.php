183b48053289689e1696b9d04fa798c04ffa41d3
Jeetix <jeetix@gmail.com>
2023-09-09 12:48:35 +0000

    Multiple worlds and multiple players

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Get or set the username
+$username = isset($_GET['username']) ? $_GET['username'] : 'defaultUser';
+$playerFile = "players/{$username}.txt";
+
+// Initialize or read player data
+if (!file_exists($playerFile)) {
+    $playerData = ['world' => 'world', 'x' => 0, 'y' => 0];
+    file_put_contents($playerFile, json_encode($playerData));
+} else {
+    $playerData = json_decode(file_get_contents($playerFile), true);
+}
+
+// Extract player info
+$playerWorld = $playerData['world'];
+$_SESSION['x'] = $playerData['x'];
+$_SESSION['y'] = $playerData['y'];
 
 if (isset($_GET['getCoords']) && $_GET['getCoords'] === 'true') {
     header('Content-Type: application/json');
@@ -7,14 +27,12 @@ if (isset($_GET['getCoords']) && $_GET['getCoords'] === 'true') {
     exit;
 }
 
-$_SESSION['x'] = isset($_GET['x']) ? (int)$_GET['x'] : (isset($_SESSION['x']) ? $_SESSION['x'] : 0);
-$_SESSION['y'] = isset($_GET['y']) ? (int)$_GET['y'] : (isset($_SESSION['y']) ? $_SESSION['y'] : 0);
-
 if (!isset($_SESSION['selected'])) {
     $_SESSION['selected'] = 'none';
 }
 
-$file = 'world.txt';
+$file = "worlds/{$playerWorld}.txt";
+
 if (!file_exists($file)) {
     $world = array_fill(0, 21, array_fill(0, 21, 'grass'));
     file_put_contents($file, serialize($world));
@@ -25,10 +43,14 @@ if (!file_exists($file)) {
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $action = $_POST['action'];
 
-    if ($action === 'left') $_SESSION['x']--;
-    if ($action === 'right') $_SESSION['x']++;
-    if ($action === 'up') $_SESSION['y']--;
-    if ($action === 'down') $_SESSION['y']++;
+    if ($action === 'left')
+        $_SESSION['x']--;
+    if ($action === 'right')
+        $_SESSION['x']++;
+    if ($action === 'up')
+        $_SESSION['y']--;
+    if ($action === 'down')
+        $_SESSION['y']++;
 
     if (in_array($action, ['grass', 'water', 'stone', 'dirt', 'none'])) {
         $_SESSION['selected'] = $action;
@@ -38,75 +60,102 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $world[$_SESSION['y'] + 10][$_SESSION['x'] + 10] = $_SESSION['selected'];
         file_put_contents($file, serialize($world));
     }
+
+    // Update player data
+    $playerData['world'] = $playerWorld;
+    $playerData['x'] = $_SESSION['x'];
+    $playerData['y'] = $_SESSION['y'];
+    file_put_contents($playerFile, json_encode($playerData));
 }
 ?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>PHPCraft 2D!</title>
     <style>
-    @keyframes pulse {
-        0% {
-            opacity: 0.5;
+        @keyframes pulse {
+            0% {
+                opacity: 0.5;
+            }
+
+            50% {
+                opacity: 1;
+            }
+
+            100% {
+                opacity: 0.5;
+            }
         }
-        50% {
-            opacity: 1;
+
+        .pulsing {
+            animation: pulse 2s infinite;
+            background-color: yellowgreen;
         }
-        100% {
-            opacity: 0.5;
+
+        .table {
+            border-spacing: 0;
         }
-    }
-    .pulsing {
-        animation: pulse 2s infinite;
-        background-color: yellowgreen;
-    }
-    .table {
-        border-spacing: 0;
-    }
     </style>
     <script>
-    document.addEventListener('keydown', function(event) {
-        let action;
-        if (event.keyCode === 37) action = 'left';
-        if (event.keyCode === 38) action = 'up';
-        if (event.keyCode === 39) action = 'right';
-        if (event.keyCode === 40) action = 'down';
-        
-        if (action) {
-            handleAction(action);
-        }
-    });
-    
-    document.querySelector('form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const action = formData.get('action');
-        handleAction(action, true);
-        document.querySelector('h2').textContent = 'Selected Block: ' + action;
-    });
-    
-    function handleAction(action, isBlockChange = false) {
-        const form = new FormData();
-        form.append('action', action);
-        fetch('', {
-            method: 'POST',
-            body: form
-        }).then(() => {
-            if (!isBlockChange) {
-                fetch('?getCoords=true')
-                    .then(response => response.json())
-                    .then(data => {
-                        const newURL = `?x=${data.x}&y=${data.y}`;
-                        window.location.href = newURL;
-                    });
+        document.addEventListener('keydown', function (event) {
+            let action;
+            if (event.keyCode === 37) action = 'left';
+            if (event.keyCode === 38) action = 'up';
+            if (event.keyCode === 39) action = 'right';
+            if (event.keyCode === 40) action = 'down';
+
+            if (action) {
+                handleAction(action);
             }
         });
-    }
+
+        document.querySelector('form').addEventListener('submit', function (event) {
+            event.preventDefault();
+            const formData = new FormData(event.target);
+            const action = formData.get('action');
+            handleAction(action, true);
+            document.querySelector('h2').textContent = 'Selected Block: ' + action;
+        });
+        function handleAction(action, isBlockChange = false) {
+            const form = new FormData();
+            form.append('action', action);
+            fetch('', {
+                method: 'POST',
+                body: form
+            }).then(() => {
+                if (!isBlockChange) {
+                    fetch('?getCoords=true')
+                        .then(response => response.json())
+                        .then(data => {
+                            const urlParams = new URLSearchParams(window.location.search);
+                            const username = urlParams.get('username') || 'defaultUser';
+                            const world = urlParams.get('world') || 'worlds/world';  // Include the 'worlds' folder
+                            const newURL = `?username=${username}&world=${world}&x=${data.x}&y=${data.y}`;
+                            window.location.href = newURL;
+                        });
+                }
+            });
+        }
     </script>
 </head>
+
 <body>
     <h1>Welcome to PHPCraft 2D!</h1>
-    <h2>Selected Block: <?php echo $_SESSION['selected']; ?></h2>
+    <form method="get">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" value="<?php echo $username; ?>">
+        <button type="submit">Switch User</button>
+    </form>
+    <form method="get">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" value="<?php echo $username; ?>">
+        <label for="world">World:</label>
+        <input type="text" id="world" name="world" value="<?php echo $playerWorld; ?>">
+        <button type="submit">Go to World</button>
+    </form>
+
     <form method="post">
         <button name="action" value="none">None</button>
         <button name="action" value="grass">Grass</button>
@@ -123,7 +172,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             for ($j = $_SESSION['x'] - 10; $j <= $_SESSION['x'] + 10; $j++) {
                 $cell = $world[$i + 10][$j + 10] ?? 'void';
                 $color = $colors[$cell];
-                
+
                 if ($i === $_SESSION['y'] && $j === $_SESSION['x']) {
                     $additionalClass = ' pulsing';
                     echo "<td class='$additionalClass' style='width:20px;height:20px;background-color:$color;'>😼</td>";
@@ -136,4 +185,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         ?>
     </table>
 </body>
+
 </html>
