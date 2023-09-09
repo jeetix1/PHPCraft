706d2b70bcf7e85f0d1dbd3b45b7f94e17890a5c
Jeetix <jeetix@gmail.com>
2023-09-09 18:11:54 +0000

    Better instructions

--- index.php
+++ index.php
@@ -91,8 +91,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <h3>Instructions:</h3>
             <ul>
                 <li>Use the arrow keys to move around the world.</li>
-                <li>Click on a block type to select it.</li>
-                <li>Click on a block in the world to place the selected block. You will keep placing selected block as you move until you cnage it.</li>
+                <li>Click on a block type to select it. (This also places block at player location)</li>
+                <li>You will keep placing selected block as you move until you cnage it.</li>
             </ul>
             <h3>Tools</h3>
             <ul>
